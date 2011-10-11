4f2736dbbec05c5c1b5bab06bb83d4036a8b559e
Fuang.Cao <cavan.cfa@gmail.com>
2011-10-11 18:44:02 +0800

    1. don't install busybox symlinks 2. change rootfs config.xml Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- toolchian/build/rootfs/busybox.mk
+++ toolchian/build/rootfs/busybox.mk
@@ -1,7 +1,8 @@
 BUSYBOX_OPTION = CONFIG_PREFIX=$(ROOTFS_PATH)
+CONFIG_FILE_PATH = $(BUILD_ROOTFS)/busybox_config
+CONFIG_FILE_NAME = cavan_defconfig
 
 all:
-	$(Q)+sb2 make $(BUSYBOX_OPTION) defconfig
+	$(Q)cp $(CONFIG_FILE_PATH) configs/$(CONFIG_FILE_NAME) -av
+	$(Q)+sb2 make $(CONFIG_FILE_NAME)
 	$(Q)+sb2 make $(BUSYBOX_OPTION) install
-	$(Q)rm $(ROOTFS_PATH)/bin/sh -rfv
-	$(Q)ln -vsf bash $(ROOTFS_PATH)/bin/sh
