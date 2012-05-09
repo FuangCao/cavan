2bb3165fdbd320dbcb57ec22092534404ca10857
Fuang.Cao <cavan.cfa@gmail.com>
2012-05-09 16:35:58 +0800

    Modify scripts
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

---
+++ script/core/application.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+function cavan-read-choise()
+{
+	local def_choise choise message
+
+	if [ "$2" = "" ] || [ "$2" = "y" ] || [ "$2" = "Y" ]
+	then
+		def_choise="Y"
+		message="$1 [Y/n]? "
+	else
+		def_choise="N"
+		message="$1 [y/N]? "
+	fi
+
+	while :
+	do
+		echo -n "${message}"
+		read choise
+		[ "${choise}" ] || choise=${def_choise}
+		case ${choise} in
+			Y | y)
+				return 0 ;;
+			n | N)
+				return 1 ;;
+			*)
+				echo "Please input y/Y/n/N/Enter" ;;
+		esac
+	done
+}
+
+function cavan-cross-config()
+{
+	local command
+
+	command="./configure --prefix=/usr --build=$(gcc -dumpmachine) --target=arm-cavan-linux-gnueabi $*"
+	cavan-read-choise "Command = \"${command}\"" "Y" || return 1
+	command || return 1
+
+	return 0
+}
+
+function cavan-app-install()
+{
+	local command
+
+	if [ "$1" ]
+	then
+		command="make DESTDIR=$1 install"
+	else
+		command="make install"
+	fi
+
+	cavan-read-choise "Command = \"${command}\"" "Y" || return 1
+	command || return 1
+
+	return 0
+}
