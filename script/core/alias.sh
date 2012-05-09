ffe285e9eedde834139f2ebbf9863b36d043dc66
Fuang.Cao <cavan.cfa@gmail.com>
2012-05-09 17:03:06 +0800

    Add some alias
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- script/core/alias.sh
+++ script/core/alias.sh
@@ -7,6 +7,15 @@ alias vc='vi *.c'
 alias vs='vi *.[sS]'
 alias vm='vi [Mm]akefile'
 alias mkconfig='make menuconfig'
+alias ls='ls --color=auto'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
 
 CMD_TCP_DD_SERVER="${CAVAN_OUT_DEBUG}/cavan-tcp_dd_server"
 CMD_TFTP_DD_SERVER="${CAVAN_OUT_DEBUG}/cavan-tftp_dd_server"
