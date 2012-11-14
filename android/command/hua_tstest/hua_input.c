c9a341d5fdcdafce9133a21cb15bc0ca5291f7b8
Fuang.Cao <cavan.cfa@gmail.com>
2012-11-14 17:51:35 +0800

    android command hua_tstest add some comment
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- android/command/hua_tstest/hua_input.c
+++ android/command/hua_tstest/hua_input.c
@@ -1,3 +1,23 @@
+/*
+ * File:         hua_input.c
+ * Based on:
+ * Author:       Fuang Cao <cavan.cfa@gmail.com>
+ *
+ * Created:	  2012-11-14
+ * Description:  HUAMOBILE INPUT DEVICE TEST
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 #include "hua_input.h"
 #include <stdarg.h>
 
