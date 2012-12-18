c848b8523a119859bea1e62d4787a65d8f0aa697
Fuang.Cao <cavan.cfa@gmail.com>
2012-12-18 13:08:59 +0800

    fixup a compile fault
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- include/cavan.h
+++ include/cavan.h
@@ -3,7 +3,14 @@
 #define _LARGEFILE64_SOURCE
 #define _FILE_OFFSET_BITS	64
 #define __USE_FILE_OFFSET64
+
+#ifndef _XOPEN_SOURCE
 #define _XOPEN_SOURCE		500
+#endif
+
+#ifndef _BSD_SOURCE
+#define _BSD_SOURCE
+#endif
 
 #include <linux/version.h>
 #include <assert.h>
