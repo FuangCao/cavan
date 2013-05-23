08d5c58f14a4a3b1728c18b41a45bab01a2bce38
Fuang.Cao <cavan.cfa@gmail.com>
2013-05-23 21:26:36 +0800

    rename CavanServer to CavanServerManager
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- android/app/CavanServerManager/Android.mk
+++ android/app/CavanServerManager/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_PACKAGE_NAME := CavanServer
+LOCAL_PACKAGE_NAME := CavanServerManager
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
