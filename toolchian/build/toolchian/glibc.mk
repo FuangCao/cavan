c2c8d6ab7edfd19ba7f3480b0b0e87b05af098a4
Fuang.Cao <cavan.cfa@gmail.com>
2014-02-11 10:44:02 +0800

    update glibc config
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- toolchian/build/toolchian/glibc.mk
+++ toolchian/build/toolchian/glibc.mk
@@ -3,6 +3,7 @@ GLIBC_OPTION =	--prefix=/usr \
 				--host=$(CAVAN_TARGET_PLAT) \
 				--disable-profile \
 				--enable-add-ons \
+				--enable-obsolete-rpc \
 				--enable-kernel=$(LOWEST_KERNEL_VERSION) \
 				--with-tls \
 				--with-__thread \
@@ -11,11 +12,16 @@ GLIBC_OPTION =	--prefix=/usr \
 
 GLIBC_CONFIGPARMS = "install_root=$(SYSROOT_PATH)"
 
+ifeq ($(GLIBC_VERSION),2.16.0)
+$(shell sed -i "s/-lgcc_s//g" $(SRC_GLIBC)/Makeconfig)
+endif
+
 all:
 	$(Q)echo "$(GLIBC_CONFIGPARMS)" > configparms
 	$(Q)libc_cv_forced_unwind=yes \
 	libc_cv_c_cleanup=yes \
 	libc_cv_gnu99_inline=yes \
+	libc_cv_ctors_header=yes \
 	BUILD_CC=gcc \
 	CC=$(CAVAN_TARGET_PLAT)-gcc \
 	AR=$(CAVAN_TARGET_PLAT)-ar \
