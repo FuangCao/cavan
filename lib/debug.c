f81a6722e3502821f087d92fe19c80c885dd3ce8
Fuang.Cao <cavan.cfa@gmail.com>
2014-08-04 11:18:17 +0800

    fixup build date fault
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- lib/debug.c
+++ lib/debug.c
@@ -173,7 +173,7 @@ const char *cavan_get_build_time_string(void)
 	}
 
 	snprintf(buff, sizeof(buff), "%04d-%02d-%02d %02d:%02d:%02d",
-		time.tm_year + 1900, time.tm_mon, time.tm_mday, time.tm_hour, time.tm_min, time.tm_sec);
+		time.tm_year + 1900, time.tm_mon + 1, time.tm_mday, time.tm_hour, time.tm_min, time.tm_sec);
 
 	return buff;
 }
