b5da269552d11f2bddbea7609b0398847866285f
Fuang.Cao <cavan.cfa@gmail.com>
2014-09-11 20:28:56 +0800

    fixup rootfs compile fault
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- toolchian/build/rootfs/busybox.mk
+++ toolchian/build/rootfs/busybox.mk
@@ -2,7 +2,9 @@ BUSYBOX_OPTION = CONFIG_PREFIX=$(ROOTFS_PATH)
 CONFIG_FILE_PATH = $(BUILD_ROOTFS)/busybox_config
 CONFIG_FILE_NAME = cavan_defconfig
 
+# $(Q)cp $(CONFIG_FILE_PATH) configs/$(CONFIG_FILE_NAME) -av
+# $(Q)+sb2 make $(CONFIG_FILE_NAME)
+
 all:
-	$(Q)cp $(CONFIG_FILE_PATH) configs/$(CONFIG_FILE_NAME) -av
-	$(Q)+sb2 make $(CONFIG_FILE_NAME)
+	$(Q)sb2 make defconfig
 	$(Q)+sb2 make $(BUSYBOX_OPTION) install
