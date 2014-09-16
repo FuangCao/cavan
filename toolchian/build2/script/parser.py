6709147638ef9e7d22b86cdf9362d84f76fc750b
Fuang.Cao <cavan.cfa@gmail.com>
2014-09-16 11:29:25 +0800

    export my python to PYTHONPATH
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- toolchian/build2/script/parser.py
+++ toolchian/build2/script/parser.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 import os, sys, getopt
-sys.path.append("../../../script/python")
 import cavan_xml, cavan_command
 
 class CavanPackage(cavan_xml.CavanXmlBase):
@@ -186,95 +185,6 @@ class CavanPackageParser(cavan_command.CavanCommandBase):
 		self.mFp.close()
 
 		return result
-
-	def GenerateMakeFile(self):
-		try:
-			opts, args = getopt.getopt(sys.argv[1:], "n:N:d:D:v:V:m:M:f:F:o:O:", ["name=", "depend=", "markdir=", "function=", "output="])
-		except:
-			ShowUsage()
-			sys.exit(-1)
-
-		if len(args) < 1:
-			ShowUsage()
-			return -1
-
-		NameMakefilePath = None
-		DependMakefilePath = None
-		MarkDir = None
-		InstallFunction = None
-
-		for opt in opts:
-			if opt[0] in ["-n", "-N", "--name"]:
-				NameMakefilePath = opt[1]
-			elif opt[0] in ["-d", "-D", "--depend"]:
-				DependMakefilePath = opt[1]
-			elif opt[0] in ["-m", "-M", "--markdir"]:
-				MarkDir = opt[1]
-			elif opt[0] in ["-f", "-F", "--function"]:
-				InstallFunction = opt[1]
-			elif opt[0] in ["-o", "-O", "--output"]:
-				NameMakefilePath = opt[1] + "/name.mk"
-				DependMakefilePath = opt[1] + "/depend.mk"
-			else:
-				print "unknown option: " + opt[0]
-				return -1
-		try:
-			xmlConfig = parse(args[0])
-			tagPackages = xmlConfig.getElementsByTagName("packages")
-		except:
-			print "parse file \"" + args[0] + "\" failed"
-			return -1
-
-		if NameMakefilePath == None or DependMakefilePath == None or MarkDir == None or InstallFunction == None:
-			ShowUsage()
-			return -1
-
-		listAppNames = []
-		listAppDepends = []
-
-		for tagPackage in tagPackages[0].getElementsByTagName("package"):
-			name = tagPackage.getAttribute("name")
-			version = tagPackage.getAttribute("version")
-			prefix = name.upper() + "_"
-			lastname = prefix + "NAME"
-			lastversion = prefix + "VERSION"
-
-			listAppNames.append(lastversion + " ?= " + version + "\n")
-			listAppNames.append(lastname + " = " + name + "-$(" + lastversion + ")\n")
-			listAppNames.append(prefix + "MARK = " + MarkDir + "/$(" + lastname + ")\n")
-			listAppNames.append(prefix + "URL = " + tagPackage.getAttribute("url") + "\n")
-			listAppNames.append(prefix + "CONFIG = " + tagPackage.getAttribute("config") + "\n")
-			listAppNames.append(prefix + "TYPE = " + tagPackage.getAttribute("type") + "\n")
-			depends = prefix + "DEPEND ="
-			for depend in tagPackage.getAttribute("depend").split(" "):
-				depend = depend.strip()
-				if depend:
-					depends += " $(" + depend.upper() + "_MARK)"
-			listAppNames.append(depends + "\n");
-			listAppNames.append("PACKAGES_ALL += $(" + prefix + "MARK)\n\n")
-
-			listAppDepends.append("$(" + prefix + "MARK): " + "$(" + prefix + "DEPEND)\n")
-			listAppDepends.append("\t$(call %s,$(%sCONFIG),$(%sURL),$(%sTYPE))\n\n" % (InstallFunction, prefix, prefix, prefix))
-
-		listAppNames.append("all: $(PACKAGES_ALL)")
-
-		try:
-			fd = open(NameMakefilePath, "w")
-		except:
-			print "failed to open file \"" + NameMakefilePath + "\""
-			return -1
-		fd.writelines(listAppNames)
-		fd.close()
-
-		try:
-			fd = open(DependMakefilePath, "w")
-		except:
-			print "failed to open file \"" + DependMakefilePath + "\""
-			return -1
-		fd.writelines(listAppDepends)
-		fd.close()
-
-		return 0
 
 if __name__ == "__main__":
 	parser = CavanPackageParser()
