35c7728f49fd3dbc7fbe0ef08cfeed3c61f61155
Fuang.Cao <cavan.cfa@gmail.com>
2015-01-22 10:14:05 +0800

    [alc5671_clock] support find config
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- app/alc5671_clock.c
+++ app/alc5671_clock.c
@@ -19,14 +19,87 @@
 
 #include <cavan.h>
 
+#define ALC5671_PLL_K_MAX	0x1F
+#define ALC5671_PLL_N_MAX	0x1FF
+#define ALC5671_PLL_M_MAX	0x0F
+
+struct alc5671_pll_config {
+	bool bypass;
+	u16 M, N, K;
+};
+
 static void show_usage(const char *command)
 {
 	println("Usage: %s MCLK M N [K]", command);
 }
 
-static inline double alc5671_cal_pll(double MCLK, int M, int N, int K)
+static inline double alc5671_pll_cal(double MCLK, int M, int N, int K)
 {
+#if 0
 	return (MCLK * (N + 2)) / ((M + 2) * (K + 2));
+#else
+	return (MCLK * (N + 2)) / (M + 2) / (K + 2);
+#endif
+}
+
+static u32 alc5671_pll_find_config(u32 MCLK, u32 OUT, struct alc5671_pll_config *config)
+{
+	int M1, N1, K1;
+	u32 diff_min = OUT;
+
+	for (K1 = 2; K1 <= ALC5671_PLL_K_MAX + 2; K1++)
+	{
+		N1 = OUT * K1 / MCLK;
+		if (N1 < 2)
+		{
+			N1 = 2;
+		}
+
+		for (; N1 <= ALC5671_PLL_N_MAX + 2; N1++)
+		{
+			u32 diff;
+			u32 O1 = MCLK * N1 / K1;
+
+			M1 = O1 / OUT;
+			if (M1 < 1 || M1 > ALC5671_PLL_M_MAX + 2)
+			{
+				continue;
+			}
+
+			O1 /= M1;
+
+			diff = O1 > OUT ? O1 - OUT : OUT - O1;
+			if (diff <= diff_min)
+			{
+				println("M1 = %d, N1 = %d, K1 = %d, diff = %d", M1, N1, K1, diff);
+
+				if (diff < diff_min)
+				{
+					config->N = N1 - 2;
+					config->K = K1 - 2;
+					if (M1 < 2)
+					{
+						config->bypass = true;
+						config->M = 0;
+					}
+					else
+					{
+						config->bypass = false;
+						config->M = M1 - 2;
+					}
+
+					if (diff == 0)
+					{
+						return 0;
+					}
+
+					diff_min = diff;
+				}
+			}
+		}
+	}
+
+	return diff_min;
 }
 
 int main(int argc, char *argv[])
@@ -44,8 +117,22 @@ int main(int argc, char *argv[])
 		N = text2value_unsigned(argv[3], NULL, 10);
 		K = argc > 4 ? text2value_unsigned(argv[4], NULL, 10) : 2;
 
-		OUT = alc5671_cal_pll(MCLK, M, N, K);
+		OUT = alc5671_pll_cal(MCLK, M, N, K);
 		println("%s => %s", frequency_tostring(MCLK, buff_in, sizeof(buff_in), NULL), frequency_tostring(OUT, buff_out, sizeof(buff_out), NULL));
+	}
+	else if (argc > 2)
+	{
+		double OUT;
+		char buff_in[1024];
+		char buff_out[1024];
+		struct alc5671_pll_config config;
+
+		MCLK = text2frequency(argv[1], NULL, NULL);
+		OUT = text2frequency(argv[2], NULL, NULL);
+		println("%s => %s", frequency_tostring(MCLK, buff_in, sizeof(buff_in), NULL), frequency_tostring(OUT, buff_out, sizeof(buff_out), NULL));
+
+		alc5671_pll_find_config(MCLK, OUT, &config);
+		println("M = %d, N = %d, K = %d, bypass = %d", config.M, config.N, config.K, config.bypass);
 	}
 	else
 	{
