4b25973c2fc00154a0c0f669f056fb860a8b0889
Fuang.Cao <cavan.cfa@gmail.com>
2015-07-01 21:54:16 +0800

    update RadixConverter
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- android/app/RadixConverter/src/com/cavan/radixconverter/MainActivity.java
+++ android/app/RadixConverter/src/com/cavan/radixconverter/MainActivity.java
@@ -5,25 +5,36 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.Spinner;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements TextWatcher, OnItemSelectedListener {
 
 	private static final int MAX_COUNT_BIN = 32;
 	private static final int MAX_COUNT_HEX = 8;
 	private static final int MAX_COUNT_DEC = 10;
 	private static final int MAX_COUNT_OCT = 11;
 
+	private static final int[] PREFIX_BASE_MAP = { 10, 16, 2, 8 };
+
 	private GridView mGridViewBin;
 	private GridView mGridViewHex;
 	private GridView mGridViewOct;
 	private GridView mGridViewDec;
 	private GridView mGridViewKeypad;
+	private EditText mEditTextNum;
+	private Spinner mSpinnerPrefix;
 
 	private BitAdapter mAdapterBin;
 	private BitAdapter mAdapterHex;
@@ -31,6 +42,7 @@ public class MainActivity extends Activity {
 	private BitAdapter mAdapterOct;
 	private KeypadAdapter mAdapterKeypad;
 
+	private long mValue;
 	private BitView mCurrBitView;
 	private List<BitAdapter> mAdapters = new ArrayList<BitAdapter>();
 
@@ -62,6 +74,7 @@ public class MainActivity extends Activity {
 			}
 
 			view.setActive(true);
+			updateValue(mValue, adapter.getBase());
 		} else {
 			mAdapterBin.setActiveViews(0, 0);
 		}
@@ -69,15 +82,29 @@ public class MainActivity extends Activity {
 		mCurrBitView = view;
 	}
 
-	private void updateValue(BitAdapter adapter) {
+	private void updateValue(long value, int base) {
+		setBase(base);
+		mEditTextNum.setText(Long.toString(value, base));
+	}
 
-		long value = adapter.getValue();
+	private void updateValue(BitAdapter adapter, long value) {
+
+		if (adapter != null) {
+			value = adapter.getValue();
+			updateValue(value, adapter.getBase());
+		}
+
+		if (value == mValue) {
+			return;
+		}
 
 		for (BitAdapter adp : mAdapters) {
 			if (adp != adapter) {
 				adp.setValue(value);
 			}
 		}
+
+		mValue = value;
 	}
 
 	private OnClickListener mBitClickListener = new OnClickListener() {
@@ -88,7 +115,7 @@ public class MainActivity extends Activity {
 
 			if (view == mCurrBitView) {
 				view.add(1);
-				updateValue(view.getAdapter());
+				updateValue(view.getAdapter(), 0);
 			} else {
 				setCurrBit((BitView) v);
 			}
@@ -106,7 +133,7 @@ public class MainActivity extends Activity {
 			Button button = (Button) v;
 			if (!button.getText().equals(mCurrBitView.getText())) {
 				mCurrBitView.setText(button.getText());
-				updateValue(mCurrBitView.getAdapter());
+				updateValue(mCurrBitView.getAdapter(), 0);
 			}
 
 			setCurrBit(mCurrBitView.getNextView());
@@ -141,6 +168,15 @@ public class MainActivity extends Activity {
 		mGridViewKeypad = (GridView) findViewById(R.id.gridViewKeypad);
 		mAdapterKeypad = new KeypadAdapter(getApplicationContext(), mKeypadClickListener);
 		mGridViewKeypad.setAdapter(mAdapterKeypad);
+
+		mEditTextNum = (EditText) findViewById(R.id.editTextNumber);
+		mEditTextNum.addTextChangedListener(this);
+
+		mSpinnerPrefix = (Spinner) findViewById(R.id.spinnerPrefix);
+		String[] prefixList = getResources().getStringArray(R.array.num_prefix);
+		ArrayAdapter<String> prefixAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, prefixList);
+		mSpinnerPrefix.setAdapter(prefixAdapter);
+		mSpinnerPrefix.setOnItemSelectedListener(this);
 	}
 
 	@Override
@@ -160,5 +196,44 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	private int getBase() {
+		int index = mSpinnerPrefix.getSelectedItemPosition();
+		return PREFIX_BASE_MAP[index];
+	}
+
+	private void setBase(int base) {
+		for (int i = 0; i < PREFIX_BASE_MAP.length; i++) {
+			if (PREFIX_BASE_MAP[i] == base) {
+				mSpinnerPrefix.setSelection(i);
+			}
+		}
+	}
+
+	@Override
+	public void afterTextChanged(Editable s) {
+	}
+
+	@Override
+	public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+	}
+
+	@Override
+	public void onTextChanged(CharSequence s, int start, int before, int count) {
+		try {
+			long value = Long.parseLong(mEditTextNum.getText().toString(), getBase());
+			updateValue(null, value);
+		} catch (Exception e) {
+		}
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		updateValue(mValue, getBase());
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
 	}
 }
