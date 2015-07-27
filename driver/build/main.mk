6d4481583a4f3e638596d3b3e1157aa66edb942f
Fuang.Cao <cavan.cfa@gmail.com>
2015-07-27 16:33:40 +0800

    fixup driver include path fault
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- driver/build/main.mk
+++ driver/build/main.mk
@@ -8,7 +8,7 @@ KERNEL_PATH = /lib/modules/$(shell uname -r)/build
 CROSS_COMPILE =
 endif
 
-CFLAGS = -Wall -Werror -I$(PWD)/include/ -I$(KERNEL_PATH)/drivers/spi/
+CFLAGS = -Wall -Werror -I $(PWD)/include/ -I drivers/spi/
 
 all:
 	+make ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_COMPILE) -C $(KERNEL_PATH) M=$(PWD) EXTRA_CFLAGS="$(CFLAGS)"
