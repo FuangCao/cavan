8ed0b419810f83df14bb8a2d4fea13f8c1d1689c
Fuang.Cao <cavan.cfa@gmail.com>
2015-08-16 00:31:39 +0800

    cavan-init support config git https
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- script/core/alias.sh
+++ script/core/alias.sh
@@ -211,11 +211,12 @@ function cavan-git-config()
 	${CMD_GIT_CONFIG} user.email cavan.cfa@gmail.com || return 1
 	${CMD_GIT_CONFIG} core.editor vim || return 1
 	${CMD_GIT_CONFIG} push.default matching || return 1
+	${CMD_GIT_CONFIG} http.sslverify false || return 1
 
 	for cmd in branch  interactive ui diff grep status
 	do
 		echo "git config color.${cmd} auto"
-		git config --global color.${cmd} auto;
+		${CMD_GIT_CONFIG} color.${cmd} auto;
 	done
 }
 
