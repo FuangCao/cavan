1ea2b830146f32a401a6f7a5dbcf64734833bb03
Fuang.Cao <cavan.cfa@gmail.com>
2015-08-26 13:18:48 +0800

    add some macro for compiler
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- include/cavan.h
+++ include/cavan.h
@@ -24,22 +24,82 @@
 #include <time.h>
 #include <unistd.h>
 
-#define GCC_VERSION		(__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
-
-#if GCC_VERSION >= 30300
-#define __used			__attribute__((__used__))
+#ifdef __GNUC__
+#define GCC_VERSION				(__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+#define	__GNUC_PREREQ__(x, y)	((__GNUC__ == (x) && __GNUC_MINOR__ >= (y)) || (__GNUC__ > (x)))
 #else
-#define __used			__attribute__((__unused__))
+#define GCC_VERSION				0
+#define	__GNUC_PREREQ__(x, y)	0
 #endif
 
-#if GCC_VERSION >= 30400
-#define __must_check	__attribute__((warn_unused_result))
+#if __GNUC_PREREQ__(2, 7)
+#define	__unused				__attribute__((__unused__))
+#else
+#define	__unused	/* delete */
+#endif
+
+#if __GNUC_PREREQ__(3, 1)
+#define	__used					__attribute__((__used__))
+#else
+#define	__used		/* delete */
+#endif
+
+#if __GNUC_PREREQ__(2, 7)
+#define	__packed				__attribute__((__packed__))
+#define	__aligned(x)			__attribute__((__aligned__(x)))
+#define	__section(x)			__attribute__((__section__(x)))
+#elif defined(__lint__)
+#define	__packed	/* delete */
+#define	__aligned(x)	/* delete */
+#define	__section(x)	/* delete */
+#else
+#define	__packed				error: no __packed for this compiler
+#define	__aligned(x)			error: no __aligned for this compiler
+#define	__section(x)			error: no __section for this compiler
+#endif
+
+#if __GNUC_PREREQ__(2, 96)
+#define	__predict_true(exp)		__builtin_expect((exp) != 0, 1)
+#define	__predict_false(exp)	__builtin_expect((exp) != 0, 0)
+#else
+#define	__predict_true(exp)		(exp)
+#define	__predict_false(exp)	(exp)
+#endif
+
+#if __GNUC_PREREQ__(2, 96)
+#define __noreturn    			__attribute__((__noreturn__))
+#define __mallocfunc  			__attribute__((malloc))
+#else
+#define __noreturn
+#define __mallocfunc
+#endif
+
+#if __GNUC_PREREQ__(3, 4)
+#define __must_check			__attribute__((warn_unused_result))
 #else
 #define __must_check
 #endif
 
-#define likely(exp)		(__builtin_expect(!!(exp), true))
-#define unlikely(exp)	(__builtin_expect(!!(exp), false))
+#if __GNUC_PREREQ__(2, 95)
+#define	__insn_barrier()		__asm __volatile("":::"memory")
+#else
+#define	__insn_barrier()
+#endif
+
+#define likely(exp)				__predict_true(exp)
+#define unlikely(exp)			__predict_false(exp)
+
+#if defined(__cplusplus)
+#define	__BEGIN_DECLS			extern "C" {
+#define	__END_DECLS				}
+#define	__static_cast(x, y)		static_cast<x>(y)
+#else
+#define	__BEGIN_DECLS
+#define	__END_DECLS
+#define	__static_cast(x, y)		((x) (y))
+#endif
+
+#define __UNCONST(a)			((void *)(unsigned long)(const void *) (a))
 
 static inline void msleep(useconds_t msec)
 {
