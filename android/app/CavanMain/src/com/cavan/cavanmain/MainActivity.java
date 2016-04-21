a01c0e1f3cb73e916f496200735cde8e62a15b4d
Fuang.Cao <cavan.cfa@gmail.com>
2016-04-21 16:53:07 +0800

    CavanMain support tcp_dd_server
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- android/app/CavanMain/src/com/cavan/cavanmain/MainActivity.java
+++ android/app/CavanMain/src/com/cavan/cavanmain/MainActivity.java
@@ -1,40 +1,61 @@
 package com.cavan.cavanmain;
 
-import com.cavan.cavanutils.CavanNative;
-import com.cavan.cavanutils.SuClient;
-
-import android.support.v7.app.ActionBarActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.cavan.cavanutils.CavanNative;
+import com.cavan.cavanutils.CavanUtils;
 
 @SuppressWarnings("deprecation")
-@SuppressLint("NewApi") public class MainActivity extends ActionBarActivity {
+@SuppressLint({ "NewApi", "HandlerLeak" }) public class MainActivity extends ActionBarActivity {
 
 	public static final String TAG = "Cavan";
+
+	private static final int EVENT_TCP_DD_SERVICE_STATE_CHANGED = 1;
+
+	private TcpDdThread mTcpDdThread;
+	private Button mButtonTcpDdService;
+
+	private Handler mHandler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case EVENT_TCP_DD_SERVICE_STATE_CHANGED:
+				if (mTcpDdThread == null) {
+					mButtonTcpDdService.setText(R.string.text_start);
+				} else {
+					mButtonTcpDdService.setText(R.string.text_stop);
+				}
+				break;
+			}
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		int res = CavanNative.doSu("-c", "pwd");
-		Log.d(TAG, "res = " + res);
-
-		new Thread() {
+		mButtonTcpDdService = (Button) findViewById(R.id.buttonTcpDdService);
+		mButtonTcpDdService.setOnClickListener(new OnClickListener() {
 
 			@Override
-			public void run() {
-				SuClient client = new SuClient();
-				client.runCommand("busybox whoami");
-				client.runCommand("busybox ls -lh");
-				client.runCommand("pm install -r \"/data/当贝市场.apk\"");
-				client.runCommand("echo \"xxxx 中文测试 xxxx\"");
-				client.runCommand("echo 123456");
+			public void onClick(View v) {
+				if (mTcpDdThread == null) {
+					mTcpDdThread = new TcpDdThread();
+					mTcpDdThread.start();
+				}
 			}
-		}.start();
+		});
 	}
 
 	@Override
@@ -54,5 +75,17 @@ import android.view.MenuItem;
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	class TcpDdThread extends Thread {
+		@Override
+		public void run() {
+			CavanUtils.logD("doTcpDdServer running");
+			mHandler.sendEmptyMessage(EVENT_TCP_DD_SERVICE_STATE_CHANGED);
+			CavanNative.doTcpDdServer("-p", "9999", "-s", "0");
+			mTcpDdThread = null;
+			mHandler.sendEmptyMessage(EVENT_TCP_DD_SERVICE_STATE_CHANGED);
+			CavanUtils.logD("doTcpDdServer stopped");
+		}
 	}
 }
