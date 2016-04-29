a0af91fcc8f7cd5bbb652e07830d9c7c9fa1a548
Fuang.Cao <cavan.cfa@gmail.com>
2016-04-29 14:01:43 +0800

    add bluetooth files
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- android/app/BluetoothSetting/Android.mk
+++ android/app/BluetoothSetting/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13 jsr305
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
