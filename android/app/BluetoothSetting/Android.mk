e9f69ef9ffcab481a6ccbb85427823d5631ac034
fuang <fuang@DESKTOP-VSGNBAP>
2016-04-29 11:16:57 +0800

    add android app BluetoothSetting
    Signed-off-by: fuang <fuang@DESKTOP-VSGNBAP>

--- android/app/BluetoothSetting/Android.mk
+++ android/app/BluetoothSetting/Android.mk
@@ -2,10 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 android-support-v13 jsr305
 
-LOCAL_PACKAGE_NAME := HuaHardwareInfo
+LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
+LOCAL_PACKAGE_NAME := BluetoothSetting
 
 include $(BUILD_PACKAGE)
