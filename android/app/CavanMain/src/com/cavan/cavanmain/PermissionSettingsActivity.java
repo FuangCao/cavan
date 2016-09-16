dac0b8b0452e422bae22a5e5cdea17c056df2d8b
Fuang.Cao <cavan.cfa@gmail.com>
2016-09-16 15:54:37 +0800

    CavanMain add some setting
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- android/app/CavanMain/src/com/cavan/cavanmain/PermissionSettingsActivity.java
+++ android/app/CavanMain/src/com/cavan/cavanmain/PermissionSettingsActivity.java
@@ -16,10 +16,12 @@ public class PermissionSettingsActivity extends PreferenceActivity {
 	private static final String KEY_PROTECTED_APPS = "protected_apps";
 	private static final String KEY_NOTIFICATION_ACCESS = "notification_access";
 	private static final String KEY_NOTIFICATION_MANAGER = "notification_manager";
+	private static final String KEY_ACCESSIBILITY_SETTINGS = "accessibility_settings";
 
 	private Preference mPreferenceProtectedApps;
 	private Preference mPreferenceNotificationAccess;
 	private Preference mPreferenceNotificationManager;
+	private Preference mPreferenceAccessibilitySettings;
 
 	@SuppressWarnings("deprecation")
 	@Override
@@ -30,8 +32,10 @@ public class PermissionSettingsActivity extends PreferenceActivity {
 		mPreferenceProtectedApps = findPreference(KEY_PROTECTED_APPS);
 		mPreferenceNotificationAccess = findPreference(KEY_NOTIFICATION_ACCESS);
 		mPreferenceNotificationManager = findPreference(KEY_NOTIFICATION_MANAGER);
+		mPreferenceAccessibilitySettings = findPreference(KEY_ACCESSIBILITY_SETTINGS);
 
 		mPreferenceNotificationAccess.setIntent(getNotificationAccessIntent());
+		mPreferenceAccessibilitySettings.setIntent(getAccessibilitySettingsIntent());
 
 		if (CavanAndroid.isHuaweiPhone()) {
 			mPreferenceProtectedApps.setIntent(new Intent(ACTION_HSM_PROTECTED_APPS));
@@ -41,6 +45,10 @@ public class PermissionSettingsActivity extends PreferenceActivity {
 
 	public static Intent getNotificationAccessIntent() {
 		return new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+	}
+
+	public static Intent getAccessibilitySettingsIntent() {
+		return new Intent("android.settings.ACCESSIBILITY_SETTINGS");
 	}
 
 	public static void startNotificationListenerSettingsActivity(Context context) {
