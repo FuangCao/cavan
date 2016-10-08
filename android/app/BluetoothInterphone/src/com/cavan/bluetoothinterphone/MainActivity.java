d7b1258031e999638376de700ac580e9b1da0c26
Fuang.Cao <cavan.cfa@gmail.com>
2016-10-08 21:46:47 +0800

    add android app BluetoothInterphone
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

---
+++ android/app/BluetoothInterphone/src/com/cavan/bluetoothinterphone/MainActivity.java
@@ -0,0 +1,45 @@
+package com.cavan.bluetoothinterphone;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	private AudioManager mManager;
+
+	private Button mButtonStop;
+	private Button mButtonStart;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mButtonStop = (Button) findViewById(R.id.buttonStop);
+		mButtonStop.setOnClickListener(this);
+
+		mButtonStart = (Button) findViewById(R.id.buttonStart);
+		mButtonStart.setOnClickListener(this);
+
+		mManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v == mButtonStart) {
+			mManager.setBluetoothScoOn(true);
+			mManager.startBluetoothSco();
+			mManager.setMode(AudioManager.MODE_IN_CALL);
+		} else if (v == mButtonStop) {
+			mManager.setMode(AudioManager.MODE_NORMAL);
+			mManager.stopBluetoothSco();
+			mManager.setBluetoothScoOn(false);
+		}
+	}
+}
