d974fca37df37483a9828f4d8cc2be0332e1bfd5
Fuang.Cao <cavan.cfa@gmail.com>
2016-10-21 20:11:05 +0800

    update JwaooOtpProgrammer
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- c#/JwaooOtpProgrammer/JwaooOtpProgrammer/Form1.Designer.cs
+++ c#/JwaooOtpProgrammer/JwaooOtpProgrammer/Form1.Designer.cs
@@ -23,26 +23,21 @@
         /// 使用代码编辑器修改此方法的内容。
         /// </summary>
         private void InitializeComponent() {
-            this.folderBrowserDialogFirmware = new System.Windows.Forms.FolderBrowserDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.textBoxFirmware = new System.Windows.Forms.TextBox();
             this.buttonFirmware = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.buttonConnect = new System.Windows.Forms.Button();
             this.buttonBurn = new System.Windows.Forms.Button();
-            this.textBoxLog = new System.Windows.Forms.TextBox();
-            this.buttonLogClean = new System.Windows.Forms.Button();
+            this.textBoxBdAddress = new System.Windows.Forms.TextBox();
+            this.openFileDialogFirmware = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialogSmartSnippets = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
-            // 
-            // folderBrowserDialogFirmware
-            // 
-            this.folderBrowserDialogFirmware.ShowNewFolderButton = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 476);
+            this.label1.Location = new System.Drawing.Point(12, 55);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 0;
@@ -50,14 +45,14 @@
             // 
             // textBoxFirmware
             // 
-            this.textBoxFirmware.Location = new System.Drawing.Point(88, 473);
+            this.textBoxFirmware.Location = new System.Drawing.Point(83, 50);
             this.textBoxFirmware.Name = "textBoxFirmware";
-            this.textBoxFirmware.Size = new System.Drawing.Size(738, 21);
+            this.textBoxFirmware.Size = new System.Drawing.Size(227, 21);
             this.textBoxFirmware.TabIndex = 1;
             // 
             // buttonFirmware
             // 
-            this.buttonFirmware.Location = new System.Drawing.Point(832, 471);
+            this.buttonFirmware.Location = new System.Drawing.Point(316, 50);
             this.buttonFirmware.Name = "buttonFirmware";
             this.buttonFirmware.Size = new System.Drawing.Size(47, 23);
             this.buttonFirmware.TabIndex = 2;
@@ -68,22 +63,15 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(17, 438);
+            this.label2.Location = new System.Drawing.Point(12, 15);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(65, 12);
             this.label2.TabIndex = 3;
-            this.label2.Text = "硬件地址：";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(91, 435);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(788, 21);
-            this.textBox1.TabIndex = 4;
+            this.label2.Text = "MAC 地址：";
             // 
             // buttonConnect
             // 
-            this.buttonConnect.Location = new System.Drawing.Point(12, 524);
+            this.buttonConnect.Location = new System.Drawing.Point(5, 103);
             this.buttonConnect.Name = "buttonConnect";
             this.buttonConnect.Size = new System.Drawing.Size(75, 23);
             this.buttonConnect.TabIndex = 5;
@@ -93,7 +81,7 @@
             // 
             // buttonBurn
             // 
-            this.buttonBurn.Location = new System.Drawing.Point(802, 524);
+            this.buttonBurn.Location = new System.Drawing.Point(288, 103);
             this.buttonBurn.Name = "buttonBurn";
             this.buttonBurn.Size = new System.Drawing.Size(75, 23);
             this.buttonBurn.TabIndex = 6;
@@ -101,35 +89,33 @@
             this.buttonBurn.UseVisualStyleBackColor = true;
             this.buttonBurn.Click += new System.EventHandler(this.buttonBurn_Click);
             // 
-            // textBoxLog
+            // textBoxBdAddress
             // 
-            this.textBoxLog.Location = new System.Drawing.Point(12, 12);
-            this.textBoxLog.Multiline = true;
-            this.textBoxLog.Name = "textBoxLog";
-            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxLog.Size = new System.Drawing.Size(865, 408);
-            this.textBoxLog.TabIndex = 7;
+            this.textBoxBdAddress.Location = new System.Drawing.Point(83, 12);
+            this.textBoxBdAddress.Name = "textBoxBdAddress";
+            this.textBoxBdAddress.Size = new System.Drawing.Size(280, 21);
+            this.textBoxBdAddress.TabIndex = 4;
             // 
-            // buttonLogClean
+            // openFileDialogFirmware
             // 
-            this.buttonLogClean.Location = new System.Drawing.Point(384, 524);
-            this.buttonLogClean.Name = "buttonLogClean";
-            this.buttonLogClean.Size = new System.Drawing.Size(75, 23);
-            this.buttonLogClean.TabIndex = 8;
-            this.buttonLogClean.Text = "清空消息";
-            this.buttonLogClean.UseVisualStyleBackColor = true;
-            this.buttonLogClean.Click += new System.EventHandler(this.buttonLogClean_Click);
+            this.openFileDialogFirmware.FileName = "jwaoo-toy.hex";
+            this.openFileDialogFirmware.Filter = "固件文件|*.hex";
+            this.openFileDialogFirmware.Title = "选择固件文件";
+            // 
+            // openFileDialogSmartSnippets
+            // 
+            this.openFileDialogSmartSnippets.FileName = "SmartSnippets.exe";
+            this.openFileDialogSmartSnippets.Filter = "SmartSnippets|SmartSnippets.exe";
+            this.openFileDialogSmartSnippets.Title = "选择文件 SmartSnippets.exe";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(889, 559);
-            this.Controls.Add(this.buttonLogClean);
-            this.Controls.Add(this.textBoxLog);
+            this.ClientSize = new System.Drawing.Size(375, 136);
             this.Controls.Add(this.buttonBurn);
             this.Controls.Add(this.buttonConnect);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxBdAddress);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.buttonFirmware);
             this.Controls.Add(this.textBoxFirmware);
@@ -142,17 +128,15 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialogFirmware;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBoxFirmware;
         private System.Windows.Forms.Button buttonFirmware;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button buttonConnect;
         private System.Windows.Forms.Button buttonBurn;
-        private System.Windows.Forms.TextBox textBoxLog;
-        private System.Windows.Forms.Button buttonLogClean;
+        private System.Windows.Forms.TextBox textBoxBdAddress;
+        private System.Windows.Forms.OpenFileDialog openFileDialogFirmware;
+        private System.Windows.Forms.OpenFileDialog openFileDialogSmartSnippets;
     }
 }
 
--- c#/JwaooOtpProgrammer/JwaooOtpProgrammer/Form1.cs
+++ c#/JwaooOtpProgrammer/JwaooOtpProgrammer/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,65 +13,207 @@ namespace JwaooOtpProgrammer {
 
     public partial class Form1 : Form {
 
-        private const String FILE_HEADER = "C:\\header.txt";
-        public const String CMD_DIR = "C:\\SmartSnippets\\bin";
-        public const String CMD_PATH = CMD_DIR + "\\SmartSnippets.exe";
-        public const String FIRMWARE_PATH = "C:\\jtag_programmer.bin";
-        public static String[] CMD_OTP_ARGS = { "-type", "otp", "-chip", "DA14580-01", "-jtag", "123456", "-baudrate", "57600", "-firmware", FIRMWARE_PATH };
+        private static String[] sProgramFiles = {
+            "C:\\Program Files (x86)", "C:\\Program Files", "D:\\Program Files (x86)", "D:\\Program Files"
+        };
+
+        private static String sBdAddrPath = Path.Combine(Application.StartupPath, "bd_address.txt");
+        private static String sProgrammerPath = Path.Combine(Application.StartupPath, "jtag_programmer.bin");
+        private static String[] sOtpCommandArgs = { "-type", "otp", "-chip", "DA14580-01", "-jtag", "123456", "-baudrate", "57600", "-firmware", sProgrammerPath };
+
+        private byte[] mBdAddress;
+        private String mSmartSnippetsPath;
 
         delegate void Process_OutputDataReceivedDelegate(object sender, DataReceivedEventArgs e);
 
         public Form1() {
             InitializeComponent();
+            openFileDialogFirmware.InitialDirectory = Application.StartupPath;
+            openFileDialogSmartSnippets.InitialDirectory = Application.StartupPath;
+
+            mBdAddress = readBdAddressFile();
+            textBoxBdAddress.Text = getBdAddressString(mBdAddress);
         }
 
-        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e) {
-            if (textBoxLog.InvokeRequired) {
-                Process_OutputDataReceivedDelegate handler = new Process_OutputDataReceivedDelegate(Process_OutputDataReceived);
-                Invoke(handler, sender, e);
-            } else {
-                textBoxLog.AppendText(e.Data + "\r\n");
+        private String getBdAddressString(byte[] bytes) {
+            if (bytes == null || bytes.Length != 6) {
+                return null;
+            }
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = bytes.Length - 1; i >= 0; i--) {
+                byte value = bytes[i];
+                builder.Append(valueToChar(value >> 4));
+                builder.Append(valueToChar(value & 0x0F));
+
+                if (i > 0) {
+                    builder.Append(':');
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        private byte[] getBdAddressBytes(String text) {
+            if (text == null) {
+                return null;
+            }
+
+            String[] texts = text.Split(':');
+            if (texts.Length != 6) {
+                return null;
+            }
+
+            byte[] bytes = new byte[6];
+            int index = bytes.Length;
+
+            foreach (String node in texts) {
+                bytes[--index] = Convert.ToByte(node, 16);
+            }
+
+            return bytes;
+        }
+
+        private byte[] readBdAddressFile() {
+            FileStream stream = null;
+
+            try {
+                stream = File.OpenRead(sBdAddrPath);
+
+                byte[] buff = new byte[32];
+                int length = stream.Read(buff, 0, buff.Length);
+                String text = Encoding.ASCII.GetString(buff, 0, length);
+
+                return getBdAddressBytes(text);
+            } catch {
+                return null;
+            } finally {
+                if (stream != null) {
+                    stream.Close();
+                }
             }
         }
 
-        private bool runSmartSnippetsCommand(params String[] args) {
-            ShellCommandRunner runner = new ShellCommandRunner(CMD_PATH, Process_OutputDataReceived, Process_OutputDataReceived);
-            runner.addArguments(args);
-            return runner.execute();
+        private bool writeBdAddressFile(byte[] bytes) {
+            String text = getBdAddressString(bytes);
+            if (text == null) {
+                return false;
+            }
+
+            bytes = Encoding.ASCII.GetBytes(text);
+
+            FileStream stream = null;
+
+            try {
+                stream = File.OpenWrite(sBdAddrPath);
+                stream.Write(bytes, 0, bytes.Length);
+                return true;
+            } catch (Exception) {
+                return false;
+            } finally {
+                if (stream != null) {
+                    stream.Close();
+                }
+            }
         }
 
-        private bool runOtpCommand(params String[] args) {
-            ShellCommandRunner runner = new ShellCommandRunner(CMD_PATH, Process_OutputDataReceived, Process_OutputDataReceived);
-            runner.addArguments(CMD_OTP_ARGS);
+        private String findSmartSnippetsPath() {
+            foreach (String path in sProgramFiles) {
+                String rootDir = Path.Combine(path, "SmartSnippets");
+                if (Directory.Exists(rootDir)) {
+                    String binDir = Path.Combine(rootDir, "bin");
+                    if (Directory.Exists(binDir)) {
+                        String smartSnippetsPath = Path.Combine(binDir, "SmartSnippets.exe");
+                        if (File.Exists(smartSnippetsPath)) {
+                            return smartSnippetsPath;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private String getSmartSnippetsPath() {
+            if (mSmartSnippetsPath != null && File.Exists(mSmartSnippetsPath)) {
+                return mSmartSnippetsPath;
+            }
+
+            mSmartSnippetsPath = findSmartSnippetsPath();
+            if (mSmartSnippetsPath != null) {
+                return mSmartSnippetsPath;
+            }
+
+            if (openFileDialogSmartSnippets.ShowDialog() != DialogResult.OK) {
+                return null;
+            }
+
+            mSmartSnippetsPath = openFileDialogSmartSnippets.FileName;
+
+            return mSmartSnippetsPath;
+        }
+
+        private ShellCommandRunner runSmartSnippetsCommand(params String[] args) {
+            ShellCommandRunner runner = new ShellCommandRunner(getSmartSnippetsPath());
             runner.addArguments(args);
-            return runner.execute();
+            if (runner.execute()) {
+                return runner;
+            }
+
+            return null;
+        }
+
+        private ShellCommandRunner runOtpCommand(params String[] args) {
+            ShellCommandRunner runner = new ShellCommandRunner(getSmartSnippetsPath());
+            runner.addArguments(sOtpCommandArgs);
+            runner.addArguments(args);
+            if (runner.execute()) {
+                return runner;
+            }
+
+            return null;
         }
 
         private bool writeOtpData(String offset, String data) {
-            return runOtpCommand("-cmd", "write_field", "-offset", offset, "-data", data);
+            ShellCommandRunner runner = runOtpCommand("-cmd", "write_field", "-offset", offset, "-data", data);
+            if (runner == null) {
+                return false;
+            }
+
+            String line = runner.LastOutputLine;
+            if (line == null) {
+                return false;
+            }
+
+            if (line.StartsWith("Failed")) {
+                return false;
+            }
+
+            return line.StartsWith("Burned");
         }
 
         private char valueToChar(int value) {
             if (value < 10) {
-                return (char) ('0' + value);
+                return (char)('0' + value);
             } else {
-                return (char) ('A' - 10 + value);
+                return (char)('A' - 10 + value);
             }
         }
 
-        private String bytesToString(byte[] bytes) {
+        private String getBytesHexString(byte[] bytes) {
             StringBuilder builder = new StringBuilder();
 
             foreach (byte value in bytes) {
+                builder.Append(valueToChar(value >> 4));
                 builder.Append(valueToChar(value & 0x0F));
-                builder.Append(valueToChar((value >> 4) & 0x0F));
             }
 
             return builder.ToString();
         }
 
         private bool writeOtpData(String offset, byte[] data) {
-            return writeOtpData(offset, bytesToString(data));
+            return writeOtpData(offset, getBytesHexString(data));
         }
 
         private bool writeBdAddress(byte[] addr) {
@@ -82,51 +225,99 @@ namespace JwaooOtpProgrammer {
         }
 
         private bool setOtpBootEnable() {
-            return writeOtpData("0x7F00", "A5A534123412A5A5");
+            return writeOtpData("0x7F00", "1234A5A5A5A51234");
         }
 
         private bool readOtpHeader(String pathname) {
-            return runOtpCommand("-cmd", "read_header", "-file", pathname);
+            ShellCommandRunner runner = runOtpCommand("-cmd", "read_header", "-file", pathname);
+            if (runner == null) {
+                return false;
+            }
+
+            String line = runner.LastOutputLine;
+            if (line == null) {
+                return false;
+            }
+
+            textBoxFirmware.Text = line;
+
+            if (line.StartsWith("Failed")) {
+                return false;
+            }
+
+            return line.StartsWith("Reading is complete");
         }
 
         private bool readOtpFirmware(String pathname) {
-            return runOtpCommand("-cmd", "read_custom_code", "-file", pathname);
+            ShellCommandRunner runner = runOtpCommand("-cmd", "read_custom_code", "-file", pathname);
+            if (runner == null) {
+                return false;
+            }
+
+            String line = runner.LastOutputLine;
+            if (line == null) {
+                return false;
+            }
+
+            textBoxFirmware.Text = line;
+
+            if (line.StartsWith("OTP memory reading has failed")) {
+                return false;
+            }
+
+            return line.StartsWith("OTP memory reading has finished");
         }
 
         private bool writeOtpFirmware(String pathname) {
-            return runOtpCommand("-cmd", "write_custom_code", "-y", "-file", pathname);
+            ShellCommandRunner runner = runOtpCommand("-cmd", "write_custom_code", "-y", "-file", pathname);
+            if (runner == null) {
+                return false;
+            }
+
+            String line = runner.LastOutputLine;
+            if (line == null) {
+                return false;
+            }
+
+            textBoxFirmware.Text = line;
+
+            if (line.StartsWith("OTP Memory burning failed")) {
+                return false;
+            }
+
+            return true;
         }
 
         private void buttonFirmware_Click(object sender, EventArgs e) {
-
+            if (openFileDialogFirmware.ShowDialog() == DialogResult.OK) {
+                textBoxFirmware.Text = openFileDialogFirmware.FileName;
+            }
         }
 
         private void buttonConnect_Click(object sender, EventArgs e) {
-            // readOtpFirmware("C:\\firmware.txt");
-            // writeBdAddress(new byte[] { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66 });
-            writeOtpFirmware("C:\\jwaoo-toy.hex");
+            bool result = false;
+            // result = readOtpHeader("C:\\header.txt");
+            result = writeBdAddress(new byte[] { 0x13, 0x12, 0x03, 0x04, 0x14, 0x25 });
+            // result = writeOtpFirmware("C:\\jwaoo-toy.hex");
+            // result = setOtpBootEnable();
+            result = readOtpFirmware("C:\\firmware.txt");
+            MessageBox.Show("result = " + result);
         }
 
         private void buttonBurn_Click(object sender, EventArgs e) {
 
-        }
-
-        private void buttonLogClean_Click(object sender, EventArgs e) {
-            textBoxLog.Clear();
         }
     }
 
     public class ShellCommandRunner {
 
         private String mCommand;
-        public DataReceivedEventHandler mErrorDataHander;
-        public DataReceivedEventHandler mOutputDataHandler;
+        private List<String> mErrLines = new List<string>();
+        private List<String> mOutLines = new List<string>();
         private List<String> mArgumants = new List<string>();
 
-        public ShellCommandRunner(String command, DataReceivedEventHandler errHandler, DataReceivedEventHandler outHandler) {
+        public ShellCommandRunner(String command) {
             mCommand = command;
-            mErrorDataHander = errHandler;
-            mOutputDataHandler = outHandler;
         }
 
         public void setArguments(List<String> args) {
@@ -158,7 +349,21 @@ namespace JwaooOtpProgrammer {
             }
         }
 
+        private String linesToString(List<String> lines) {
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < lines.Count; i++) {
+                builder.Append(i + ". " + lines[i] + "\r\n");
+            }
+
+            return builder.ToString();
+        }
+
         public bool execute() {
+            if (mCommand == null) {
+                return false;
+            }
+
             Process process = new Process();
 
             if (mArgumants != null && mArgumants.Count > 0) {
@@ -183,37 +388,84 @@ namespace JwaooOtpProgrammer {
             process.StartInfo.FileName = mCommand;
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.CreateNoWindow = true;
-            process.StartInfo.RedirectStandardInput = false;
             process.StartInfo.RedirectStandardOutput = true;
             process.StartInfo.RedirectStandardError = true;
+            process.OutputDataReceived += Process_OutputDataReceived;
+            process.ErrorDataReceived += Process_ErrorDataReceived;
 
-            if (mErrorDataHander != null) {
-                process.ErrorDataReceived += mErrorDataHander;
-            }
+            MessageBox.Show("Arguments = " + process.StartInfo.Arguments);
 
-            if (mOutputDataHandler != null) {
-                process.OutputDataReceived += mOutputDataHandler;
-            }
+            mOutLines.Clear();
+            mErrLines.Clear();
 
             try {
-                MessageBox.Show("Arguments = " + process.StartInfo.Arguments);
                 if (process.Start()) {
-                    if (mOutputDataHandler != null) {
-                        process.BeginOutputReadLine();
+                    process.BeginOutputReadLine();
+                    process.BeginErrorReadLine();
+                    process.WaitForExit();
+
+                    if (process.ExitCode != 0) {
+                        return false;
                     }
 
-                    if (mOutputDataHandler != null) {
-                        process.BeginErrorReadLine();
+                    if (mOutLines.Count > 0) {
+                        MessageBox.Show(linesToString(mOutLines));
                     }
 
-                    // process.WaitForExit();
-                    MessageBox.Show("code = " + process.ExitCode);
                     return true;
                 }
-            } catch {
+            } catch (Exception e) {
+                MessageBox.Show("Error: " + e);
             }
 
             return false;
+        }
+
+        public List<String> OutputLines {
+            get {
+                return mOutLines;
+            }
+        }
+
+        public int OutputLineCount {
+            get {
+                return mOutLines.Count;
+            }
+        }
+
+        public String LastOutputLine {
+            get {
+                int count = mOutLines.Count;
+                if (count > 0) {
+                    return mOutLines[count - 1];
+                }
+
+                return null;
+            }
+        }
+
+        public List<String> ErrorLines {
+            get {
+                return mErrLines;
+            }
+        }
+
+        public String getOutputLine(int index) {
+            return mOutLines[index];
+        }
+
+        public String getErrorLine(int index) {
+            return mErrLines[index];
+        }
+
+        private void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e) {
+            mErrLines.Add(e.Data);
+        }
+
+        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e) {
+            if (e.Data != null) {
+                mOutLines.Add(e.Data);
+            }
         }
     }
 }
