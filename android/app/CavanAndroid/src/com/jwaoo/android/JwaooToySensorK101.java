07f9d6edd9231f8e7026c569cefcff318b607f96
Fuang.Cao <cavan.cfa@gmail.com>
2016-10-27 18:49:43 +0800

    jwaoo toy support 16bit gsensor data
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- android/app/CavanAndroid/src/com/jwaoo/android/JwaooToySensorK101.java
+++ android/app/CavanAndroid/src/com/jwaoo/android/JwaooToySensorK101.java
@@ -1,9 +1,15 @@
 package com.jwaoo.android;
 
+import com.cavan.java.CavanJava;
+
 public class JwaooToySensorK101 extends JwaooToySensor {
 
 	private double readAxis(byte value) {
 		return value * 9.8 / 64;
+	}
+
+	private double readAxis(byte[] bytes, int offset) {
+		return CavanJava.buildValue16(bytes, offset) * 9.8 / 16384;
 	}
 
 	private double readCapacity(byte value, int index) {
@@ -22,10 +28,16 @@ public class JwaooToySensorK101 extends JwaooToySensor {
 
 	@Override
 	public void putBytes(byte[] bytes) {
-		setAxisX(readAxis(bytes[0]));
-		setAxisY(readAxis(bytes[1]));
-		setAxisZ(readAxis(bytes[2]));
-		setCapacity(bytes[3]);
+		if (bytes.length == 6) {
+			setAxisX(readAxis(bytes, 0));
+			setAxisY(readAxis(bytes, 2));
+			setAxisZ(readAxis(bytes, 4));
+		} else {
+			setAxisX(readAxis(bytes[0]));
+			setAxisY(readAxis(bytes[1]));
+			setAxisZ(readAxis(bytes[2]));
+			setCapacity(bytes[3]);
+		}
 	}
 
 	@Override
