fc0421c3ad358a50f39a8ac2139c30fe44268b1f
Fuang.Cao <cavan.cfa@gmail.com>
2017-01-05 15:35:50 +0800

    update GreenDaoDemo
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- android/studio/GreenDaoDemo/greendaogenerator/src/main/java/com/cavan/GreenDaoGenerator.java
+++ android/studio/GreenDaoDemo/greendaogenerator/src/main/java/com/cavan/GreenDaoGenerator.java
@@ -1,8 +1,56 @@
 package com.cavan;
 
+import java.io.File;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
 public class GreenDaoGenerator {
 
+    private File mOutDir;
+    private Schema mSchema;
+
+    public GreenDaoGenerator(String outDir) {
+        mOutDir = new File(outDir);
+        mSchema = new Schema(0, "com.cavan.greendao");
+    }
+
+    private void addEntities() {
+        Entity entity = mSchema.addEntity("UserInfo");
+        entity.addIdProperty().autoincrement();
+        entity.addStringProperty("name").index();
+        entity.addBooleanProperty("sex");
+        entity.addIntProperty("age");
+
+        entity = mSchema.addEntity("GroupInfo");
+        entity.addIdProperty().autoincrement();
+        entity.addStringProperty("name").index();
+        entity.addStringProperty("description");
+    }
+
+    public boolean generateAll() {
+        if (!mOutDir.isDirectory()) {
+            if (!mOutDir.mkdirs()) {
+                return false;
+            }
+        }
+
+        addEntities();
+
+        try {
+            DaoGenerator generator = new DaoGenerator();
+            generator.generateAll(mSchema, mOutDir.getPath());
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
-        System.out.print("11111111111111111");
+        GreenDaoGenerator generator = new GreenDaoGenerator(args[0]);
+        generator.generateAll();
     }
 }
