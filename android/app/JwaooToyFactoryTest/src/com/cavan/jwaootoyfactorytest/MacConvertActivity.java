6685a83b47e3343b9294d9333371510c0bcb6c17
Fuang.Cao <cavan.cfa@gmail.com>
2017-03-17 18:39:20 +0800

    add MacConvertActivity
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

---
+++ android/app/JwaooToyFactoryTest/src/com/cavan/jwaootoyfactorytest/MacConvertActivity.java
@@ -0,0 +1,14 @@
+package com.cavan.jwaootoyfactorytest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MacConvertActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mac_convert);
+	}
+
+}
