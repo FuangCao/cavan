1980a840bdd86cd1db8a86d8310d172fc12bdaef
Fuang.Cao <cavan.cfa@gmail.com>
2017-04-20 18:19:29 +0800

    add some script
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- script/app/git.sh
+++ script/app/git.sh
@@ -8,3 +8,12 @@ function cavan-git-force-delete()
 	git prune || return 1
 	git gc || return 1
 }
+
+function cavan-git-push()
+{
+	git remote -v | awk '{print $1}' | uniq | while read remote
+	do
+		echo "remote = ${remote}"
+		git push "${remote}" $@
+	done
+}
