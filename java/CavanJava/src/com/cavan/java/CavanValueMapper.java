c93125c003d6932aae49c7eaf2c00966fe55f12a
Fuang.Cao <cavan.cfa@gmail.com>
2017-06-03 12:33:35 +0800

    add all eclipse settings to git
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

---
+++ java/CavanJava/src/com/cavan/java/CavanValueMapper.java
@@ -0,0 +1,35 @@
+package com.cavan.java;
+
+public class CavanValueMapper {
+
+	public static class Entry {
+
+		private double mValue0;
+		private double mValue1;
+
+		public Entry(double v0, double v1) {
+			mValue1 = v0;
+			mValue1 = v1;
+		}
+
+		public double getValue0() {
+			return mValue0;
+		}
+
+		public void setValue0(double value0) {
+			mValue0 = value0;
+		}
+
+		public double getValue1() {
+			return mValue1;
+		}
+
+		public void setValue1(double value1) {
+			mValue1 = value1;
+		}
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
