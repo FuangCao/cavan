3542c15a4ed2feb706f0c37d42de93d56a7bf7d5
FuangCao <cavan.cao@foxmail.com>
2017-11-24 16:37:48 +0800

    support share clipboard
    Signed-off-by: FuangCao <cavan.cao@foxmail.com>

---
+++ c#/NetworkInputMethod/NetworkInputMethod/CavanBusyLock.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkInputMethod
+{
+    public class CavanBusyLock
+    {
+        private int mOverTime;
+        private object mLockedObj;
+        private DateTime mLockedTime = DateTime.Now;
+
+        public CavanBusyLock(int overtime)
+        {
+            mOverTime = overtime;
+        }
+
+        public CavanBusyLock()
+        {
+            mOverTime = 200;
+        }
+
+        public bool acquire(object obj)
+        {
+            DateTime now = DateTime.Now;
+
+            lock (this)
+            {
+                if (obj != mLockedObj && mLockedObj != null)
+                {
+                    TimeSpan span = now - mLockedTime;
+
+                    if (span.TotalMilliseconds < mOverTime)
+                    {
+                        return false;
+                    }
+                }
+
+                mLockedObj = obj;
+                mLockedTime = now;
+            }
+
+            return true;
+        }
+
+        public bool release(object obj)
+        {
+            lock (this)
+            {
+                if (mLockedObj == obj)
+                {
+                    mLockedObj = null;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
