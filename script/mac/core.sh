4a5b0c526103139b5bcd7be78a66c3a2ef253e1a
Fuang.Cao <cavan.cfa@gmail.com>
2017-12-06 17:32:02 +0800

    add bash script for mac
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- script/mac/core.sh
+++ script/mac/core.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 export BREW_PROFIX=$(brew --prefix)
+export FILE_GIT_COMPLETION="${HOME}/.git-completion.bash"
 
-[ -f "${BREW_PROFIX}/etc/bash_completion" ] && source "${BREW_PROFIX}/etc/bash_completion"
+for fn in ${BREW_PROFIX}/etc/bash_completion ${FILE_GIT_COMPLETION}
+do
+	[ -f "${fn}" ] && source "${fn}"
+done
 
 function cavan-mac-install-brew()
 {
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
+function cavan-mac-setup()
+{
+	brew install bash-completion
+	curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > "${FILE_GIT_COMPLETION}"
 }
