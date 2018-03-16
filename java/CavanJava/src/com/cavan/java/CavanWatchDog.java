ea2c719fd47d735a12a7de252b2e27075cef8bdf
Fuang.Cao <cavan.cfa@gmail.com>
2018-03-16 15:22:22 +0800

    add CavanWatchDog
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

---
+++ java/CavanJava/src/com/cavan/java/CavanWatchDog.java
@@ -0,0 +1,60 @@
+package com.cavan.java;
+
+public abstract class CavanWatchDog extends Thread {
+
+	private boolean mEnabled;
+	private boolean mHungry;
+	private	int mMseconds;
+
+	public CavanWatchDog(int mseconds) {
+		mMseconds = mseconds;
+	}
+
+	public synchronized boolean isEnabled() {
+		return mEnabled;
+	}
+
+	public synchronized void setEnabled(boolean enabled) {
+		mEnabled = enabled;
+		mHungry = false;
+
+		if (enabled) {
+			if (isAlive()) {
+				notify();
+			} else {
+				start();
+			}
+		}
+	}
+
+	public synchronized void feed() {
+		mHungry = false;
+	}
+
+	protected abstract void onWatchDogFire();
+
+	@Override
+	public synchronized void run() {
+		while (true) {
+			while (mEnabled) {
+				mHungry = true;
+
+				try {
+					wait(mMseconds);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				if (mHungry && mEnabled) {
+					onWatchDogFire();
+				}
+			}
+
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
