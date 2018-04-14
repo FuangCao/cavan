d4b5dfab691168149d19196642c1def1e288883f
Fuang.Cao <cavan.cfa@gmail.com>
2018-04-14 14:50:26 +0800

    update login dialog
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- android/app/CavanMain/src/com/cavan/cavanmain/CavanMainAccessibilityService.java
+++ android/app/CavanMain/src/com/cavan/cavanmain/CavanMainAccessibilityService.java
@@ -71,7 +71,7 @@ public class CavanMainAccessibilityService extends CavanAccessibilityService {
 
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-		builder.setSingleChoiceItems(items, 0, new OnClickListener() {
+		builder.setSingleChoiceItems(items, -1, new OnClickListener() {
 
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
