5a7524afd817fdcad6e589574fbd4fb9d2d20a8c
Fuang.Cao <cavan.cfa@gmail.com>
2018-12-04 14:48:40 +0800

    add some command for git
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- script/app/git.sh
+++ script/app/git.sh
@@ -17,3 +17,30 @@ function cavan-git-push()
 		git push "${remote}" $@
 	done
 }
+
+function cavan-git-tag-create()
+{
+	local tag="$1"
+
+	[ "${tag}" ] || tag="$(date +v%Y-%m-%d-%H%M%S)"
+
+	echo "tag = ${tag}"
+
+	git tag "${tag}"
+}
+
+function cavan-git-tag-push()
+{
+	cavan-git-push --tags
+}
+
+function cavan-git-tag-delete()
+{
+	local tag="$1"
+
+	echo "tag = ${tag}"
+
+	[ "${tag}" ] || return 1
+
+	git tag -d "${tag}" && cavan-git-push ":refs/tags/${tag}" || return 1
+}
