3f2c2bd6c8c8072417fcd1684835b084d7d4792e
Fuang.Cao <cavan.cfa@gmail.com>
2020-03-26 00:55:07 +0800

    update build commands
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- script/app/qcom.sh
+++ script/app/qcom.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+alias cavan-qcom-lunch-CP10="cavan-android-choosecombo 1 CP10 userdebug 32"
+alias cavan-qcom-build-CP10="cavan-qcom-lunch-CP10 && cavan-qcom-build-android"
+
 alias cavan-qcom-lunch-APH7="cavan-android-choosecombo 1 APH7 userdebug 32"
-alias cavan-qcom-build-android-APH7="cavan-qcom-lunch-APH7 && cavan-qcom-build-android"
-alias cavan-qcom-build-bootloader="make aboot -j8"
-alias cavan-qcom-build-kernel="make bootimage -j8"
-alias cavan-qcom-build-system="make systemimage -j8"
+alias cavan-qcom-build-APH7="cavan-qcom-lunch-APH7 && cavan-qcom-build-android"
+
+alias cavan-qcom-build-bootloader="(cavan-android-croot && make aboot -j8)"
+alias cavan-qcom-build-kernel="(cavan-android-croot && make bootimage -j8)"
+alias cavan-qcom-build-system="(cavan-android-croot && make systemimage -j8)"
 
 function cavan-qcom-build-android()
 {
@@ -17,6 +21,7 @@ function cavan-qcom-build-android()
 function cavan-qcom-build-modem-msm8953()
 {
 	cavan-android-croot && cd modem || return 1
+	rm -rf ADSP.8953.2.8.2/adsp_proc/obj || return 1
 	source factory/build/envsetup_sm55c72.sh || return 1
 	./build_all_8953.sh || return 1
 	./gen_firehose_8953.sh || return 1
