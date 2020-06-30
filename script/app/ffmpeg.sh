34b1b103365823e5e91f9954d43b7fb0ae346555
Fuang.Cao <cavan.cfa@gmail.com>
2020-06-30 14:47:10 +0800

    add some command
    Signed-off-by: Fuang.Cao <cavan.cfa@gmail.com>

--- script/app/ffmpeg.sh
+++ script/app/ffmpeg.sh
@@ -1,5 +1,57 @@
 #!/bin/bash
 
+function cavan-ffmpeg-u8-8000()
+{
+	local target_fn
+
+	for fn in $@
+	do
+		target_fn="${fn}-8000.u8"
+		rm -rf $target_fn* || return 1
+		ffmpeg -i $fn -ac 1 -ar 8000 -f u8 $target_fn || return 1
+		ffmpeg -ac 1 -ar 8000 -f u8 -i $target_fn ${target_fn}.wav || return 1
+	done
+}
+
+function cavan-ffmpeg-u8-16000()
+{
+	local target_fn
+
+	for fn in $@
+	do
+		target_fn="${fn}-16000.u8"
+		rm -rf $target_fn* || return 1
+		ffmpeg -i $fn -ac 1 -ar 16000 -f u8 $target_fn || return 1
+		ffmpeg -ac 1 -ar 16000 -f u8 -i $target_fn ${target_fn}.wav || return 1
+	done
+}
+
+function cavan-ffmpeg-u8-22050()
+{
+	local target_fn
+
+	for fn in $@
+	do
+		target_fn="${fn}-22050.u8"
+		rm -rf $target_fn* || return 1
+		ffmpeg -i $fn -ac 1 -ar 22050 -f u8 $target_fn || return 1
+		ffmpeg -ac 1 -ar 22050 -f u8 -i $target_fn ${target_fn}.wav || return 1
+	done
+}
+
+function cavan-ffmpeg-u8-44100()
+{
+	local target_fn
+
+	for fn in $@
+	do
+		target_fn="${fn}-44100.u8"
+		rm -rf $target_fn* || return 1
+		ffmpeg -i $fn -ac 1 -ar 44100 -f u8 $target_fn || return 1
+		ffmpeg -ac 1 -ar 44100 -f u8 -i $target_fn ${target_fn}.wav || return 1
+	done
+}
+
 function cavan-ffmpeg-u16-8000()
 {
 	local target_fn
